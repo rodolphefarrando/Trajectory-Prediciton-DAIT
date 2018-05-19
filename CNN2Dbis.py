09eb24748a4283b45329d3ad718ab8b9f72f5f3d
Romain <4romain@tsf-428-wpa-7-233.epfl.ch>
2018-05-19 16:25:38 +0200

    few changes

--- CNN2Dbis.py
+++ CNN2Dbis.py
@@ -15,22 +15,22 @@ import pdb
 
 torch.manual_seed(1)
 
-class CNN2D(nn.Module):
+class CNN2Dbis(nn.Module):
     def __init__(self, n_input_channels=1, n_output=None):
         super().__init__()
               
         self.conv1 = nn.Conv2d(n_input_channels,3,5,padding=2)
         self.conv1_bn = nn.BatchNorm2d(3, eps=1e-05, momentum=0.1, affine=True)
      
-        self.fc1 = nn.Linear(10 * 272 * 3, 10 * 4 * 1)
+        self.fc1 = nn.Linear(10 * 2 * 3, 10 * 2 * 1)
             
     def forward(self, x):
         
         x = F.relu(self.conv1_bn(self.conv1(x)))
-        x = x.view(-1, 10 * 272 * 3) # in order to reshape the tensor for as many columns we need
+        x = x.view(-1, 10 * 2 * 3) # in order to reshape the tensor for as many columns we need
         x = self.fc1(x)
  
         return x
-      
+    
     def predict(self, x):
         return self.forward(x)
