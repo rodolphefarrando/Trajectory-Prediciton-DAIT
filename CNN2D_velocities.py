d537306339bc6d9a497cd370be191afc86a69f6e
Romain <4romain@tsf-428-wpa-7-233.epfl.ch>
2018-05-20 17:46:07 +0200

    New models of CNN

--- CNN2D_velocities.py
+++ CNN2D_velocities.py
@@ -13,14 +13,14 @@ import pdb
 
 torch.manual_seed(1)
 
-class CNN2D(nn.Module):
+class CNN2D_velocities(nn.Module):
     def __init__(self, n_input_channels=1, n_output=None):
         super().__init__()
               
         self.conv1 = nn.Conv2d(n_input_channels,3,5,padding=2)
         self.conv1_bn = nn.BatchNorm2d(3, eps=1e-05, momentum=0.1, affine=True)
      
-        self.fc1 = nn.Linear(10 * 164 * 3, 10 * 4 * 1)
+        self.fc1 = nn.Linear(10 * 164 * 3, 10 * 2 * 1)
             
     def forward(self, x):
         
