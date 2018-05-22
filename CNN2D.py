e96bb5dc23595cd64af861702692fa2aa0e7880a
Romain <4romain@tsf-428-wpa-7-233.epfl.ch>
2018-05-22 11:26:18 +0200

    add elements on the table

--- CNN2D.py
+++ CNN2D.py
@@ -23,13 +23,13 @@ class CNN2D(nn.Module):
         self.conv2 = nn.Conv2d(3,10,5,padding=2)
         self.conv2_bn = nn.BatchNorm2d(10, eps=1e-05, momentum=0.1, affine=True)
      
-        self.fc1 = nn.Linear(5 * 82 * 10, 10 * 4 * 1)
+        self.fc1 = nn.Linear(5 * 122 * 10, 10 * 4 * 1)
             
     def forward(self, x):
         
         x = F.max_pool2d(F.relu(self.conv1(x)),2)
         x = F.relu(self.conv2_bn(self.conv2(x)))
-        x = x.view(-1, 5 * 82 * 10) # in order to reshape the tensor for as many columns we need
+        x = x.view(-1, 5 * 122 * 10) # in order to reshape the tensor for as many columns we need
         x = self.fc1(x)
  
         return x
